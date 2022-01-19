4ae4d489b11851554231dcb8e335a458b99acbe6
john gallagher <127highstreet@gmail.com>
2022-01-19 11:57:24 -0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Moose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
+
